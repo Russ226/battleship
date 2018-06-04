98b876dc8c4a2c2103b5ffca7b77b84c54386061
russ226 <russ2510@gmail.com>
2018-06-03 23:27:25 -0400

    working on hit registration

--- Test/com/test/hit/TestHit.java
+++ Test/com/test/hit/TestHit.java
@@ -1,4 +1,37 @@
 package com.test.hit;
 
+import com.battleship.player.Player;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestHit {
+
+    @Test
+    public void testHitRegistration(){
+        Player player = new Player();
+
+        player.insertCarrier('h', 'r', 4,4);
+        assertEquals(player.registerHit(5,4), 'h');
+    }
+
+    @Test
+    public void testHitRegistrationSinking(){
+        Player player = new Player();
+
+        player.insertCarrier('h', 'r', 4,4);
+        player.registerHit(4,4);
+        player.registerHit(5,4);
+        player.registerHit(6,4);
+        player.registerHit(7,4);
+        assertEquals(player.registerHit(8,4), 'c');
+    }
+
+    @Test
+    public void testHitRegistrationTwice(){
+        Player player = new Player();
+
+        player.insertCarrier('h', 'r', 4,4);
+        assertEquals(player.registerHit(5,4), 'h');
+        assertEquals(player.registerHit(5,4), 'm');
+    }
 }
