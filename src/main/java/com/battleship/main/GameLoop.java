1f1f19b51c9f59a5b6653d2190d7241b050a2e98
russ226 <russ2510@gmail.com>
2018-06-04 22:33:31 -0400

    finished

--- src/main/java/com/battleship/main/GameLoop.java
+++ src/main/java/com/battleship/main/GameLoop.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class GameLoop {
     private boolean won;
-    Player player1, player2;
+    private Player player1, player2;
 
 
     public GameLoop(){
@@ -17,7 +17,128 @@ public class GameLoop {
 
     }
 
-    public void player1SetUpBoard(){
+    public void MainLoop(){
+        boolean turn = false;
+        Scanner reader = new Scanner(System.in);
+        int inputX, inputY;
+        char checkPrevMoves;
+
+        System.out.println("Player 1 set up your board");
+        player1SetUpBoard();
+
+        System.out.println("Player 2 set up your board");
+        player2SetUpBoard();
+
+        while(!won){
+            // player 1's turn
+            if(!turn){
+                turn = true;
+                do{
+                    System.out.println("\n Would you like to check your previous moves?(y or n)");
+
+                    checkPrevMoves = reader.next().charAt(0);
+                    if(checkPrevMoves == 'y'){
+                        player1.getMoveBoard().printBoard();
+
+                    }
+                }while (checkPrevMoves != 'n');
+
+                do {
+                    System.out.println("Player 1 pick X coordinate: ");
+                    inputX = reader.nextInt();
+
+                    System.out.println("Player 1 pick Y coordinate: ");
+                    inputY = reader.nextInt();
+
+                    if(inputX > 10 && inputX < 0){
+                        System.out.println("Invalid X coordinate " + inputX);
+                    }
+
+                    if(inputY > 10 && inputY < 0){
+                        System.out.println("Invalid Y coordinate " + inputY);
+                    }
+
+                }while(inputX > 10 && inputX < 0 && inputX > 10 && inputX < 0);
+
+                player1.recordMove(inputX, inputY);
+                char hit = player2.registerHit(inputX,inputY);
+                if(hit == 'm'){
+                    System.out.println("Player 1 misses");
+                }else if(hit == 'h'){
+                    System.out.println("Player 1 hits a ship");
+                }else if(hit == 'b'){
+                    System.out.println("Player 1 sinks a battleship");
+                }else if(hit == 'c'){
+                    System.out.println("Player 1 sinks a carrier");
+                }else if(hit == 'k'){
+                    System.out.println("Player 1 sinks a cruiser");
+                }else if(hit == 'd'){
+                    System.out.println("Player 1 sinks a destroyer");
+                }else if(hit == 's'){
+                    System.out.println("Player 1 sinks a submarine");
+                }
+
+            }else{
+                    do{
+                        System.out.println("\n Would you like to check your previous moves?(y or n)");
+
+                        checkPrevMoves = reader.next().charAt(0);
+                        if(checkPrevMoves == 'y'){
+                            player2.getMoveBoard().printBoard();
+
+                        }
+                    }while (checkPrevMoves != 'n');
+
+                    do {
+                        System.out.println("Player 2 pick X coordinate: ");
+                        inputX = reader.nextInt();
+
+                        System.out.println("Player 2 pick Y coordinate: ");
+                        inputY = reader.nextInt();
+
+                        if(inputX > 10 && inputX < 0){
+                            System.out.println("Invalid X coordinate " + inputX);
+                        }
+
+                        if(inputY > 10 && inputY < 0){
+                            System.out.println("Invalid Y coordinate " + inputY);
+                        }
+
+                    }while(inputX > 10 && inputX < 0 && inputX > 10 && inputX < 0);
+
+                    player2.recordMove(inputX, inputY);
+                    char hit = player1.registerHit(inputX,inputY);
+                    if(hit == 'm'){
+                        System.out.println("Player 1 misses");
+                    }else if(hit == 'h'){
+                        System.out.println("Player 1 hits a ship");
+                    }else if(hit == 'b'){
+                        System.out.println("Player 1 sinks a battleship");
+                    }else if(hit == 'c'){
+                        System.out.println("Player 1 sinks a carrier");
+                    }else if(hit == 'k'){
+                        System.out.println("Player 1 sinks a cruiser");
+                    }else if(hit == 'd'){
+                        System.out.println("Player 1 sinks a destroyer");
+                    }else if(hit == 's'){
+                        System.out.println("Player 1 sinks a submarine");
+                    }
+
+
+            }
+
+            if(player1.getShipPieces() == 0){
+                won =true;
+                System.out.println("GAME OVER Player 1 wins!");
+            }if(player2.getShipPieces() == 0){
+                won =true;
+                System.out.println("GAME OVER Player 2 wins!");
+            }
+        }
+
+    }
+
+    private void player1SetUpBoard(){
         int shipChoice, col, row;
         char align, dir;
 
@@ -82,6 +203,71 @@ public class GameLoop {
 
     }
 
+    private void player2SetUpBoard(){
+        int shipChoice, col, row;
+        char align, dir;
+
+        while(player2.getShipPieces() != 5) {
+
+            System.out.println("Your board so far: ");
+            player1.getShips().printBoard();
+            System.out.println('\n');
+
+            shipChoice = chooseShip(false, player2);
+            align = chooseAlignment();
+            dir = chooseDirection(align);
+            col = chooseColumn();
+            row =  chooseRow();
+
+            if(shipChoice == 5){
+                if(!player2.insertCarrier(align, dir, col, row)){
+                    System.out.println("Unable to place ship");
+                }
+
+            }if(shipChoice == 4){
+                if(!player2.insertBattleShip(align, dir, col, row)){
+                    System.out.println("Unable to place ship");
+                }
+            }if(shipChoice == 3){
+                if(!player2.insertCruiser(align, dir, col, row)){
+                    System.out.println("Unable to place ship");
+                }
+            }if(shipChoice == 2){
+                if(!player2.insertDestroyer(align, dir, col, row)){
+                    System.out.println("Unable to place ship");
+                }
+            }if(shipChoice == 1){
+                if(!player2.insertSubmarine(align, dir, col, row)){
+                    System.out.println("Unable to place ship");
+                }
+            }
+
+        }
+
+        System.out.println("Your board: ");
+        player1.getShips().printBoard();
+
+        Scanner reader = new Scanner(System.in);
+        char choice;
+        do{
+            System.out.println("\n Would you like to change any ships?(y or n)");
+
+            choice = reader.next().charAt(0);
+            if(choice == 'y'){
+                shipChoice = chooseShip(true, player1);
+                align = chooseAlignment();
+                dir = chooseDirection(align);
+                col = chooseColumn();
+                row =  chooseRow();
+
+                player1.changeShipInsert(shipChoice,align,dir,col,row);
+
+            }
+        }while (choice != 'n');
+
+
+    }
+
 
     private int chooseShip(boolean edit, Player player){
         int shipType;
@@ -105,7 +291,7 @@ public class GameLoop {
                     System.out.println("5.Carrier");
                 }
 
-                System.out.print("chose number: ");
+                System.out.print("choose number: ");
                 shipType = reader.nextInt();
 
 
@@ -140,7 +326,7 @@ public class GameLoop {
 
     }
 
-    public char chooseAlignment(){
+    private char chooseAlignment(){
         Scanner reader = new Scanner(System.in);
         char choice;
 
@@ -154,7 +340,7 @@ public class GameLoop {
         return choice;
     }
 
-    public char chooseDirection(char align){
+    private char chooseDirection(char align){
         Scanner reader = new Scanner(System.in);
         char choice;
 
@@ -176,12 +362,12 @@ public class GameLoop {
         return choice;
     }
 
-    public int chooseColumn(){
+    private int chooseColumn(){
         Scanner reader = new Scanner(System.in);
         int choice;
 
         do{
-            System.out.print("chose X coordinate between 0  and 9");
+            System.out.print("choose X coordinate between 0  and 9");
             choice = reader.nextInt();
 
 
@@ -191,12 +377,12 @@ public class GameLoop {
         return choice;
     }
 
-    public int chooseRow(){
+    private int chooseRow(){
         Scanner reader = new Scanner(System.in);
         int choice;
 
         do{
-            System.out.print("chose Y coordinate between 0  and 9");
+            System.out.print("choose Y coordinate between 0  and 9");
             choice = reader.nextInt();
 
 
--- src/main/java/com/battleship/main/GameRunner.java
+++ src/main/java/com/battleship/main/GameRunner.java
@@ -4,6 +4,6 @@ public class GameRunner {
     public static void main(String args []){
         GameLoop gameLoop = new GameLoop();
 
-        gameLoop.player1SetUpBoard();
+        gameLoop.MainLoop();
     }
 }
